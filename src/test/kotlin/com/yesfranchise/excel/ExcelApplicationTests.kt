72b9ec97f176b9b7dc432e4e223c7d34996f3622
jwkimStation3 <38322328+JWHAPO@users.noreply.github.com>
2021-05-17 11:25:02 +0900

    first commit

---
+++ src/test/kotlin/com/yesfranchise/excel/ExcelApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yesfranchise.excel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
