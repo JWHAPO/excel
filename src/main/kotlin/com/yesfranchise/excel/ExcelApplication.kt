17e78dad86e4de851a273d55e0ef48d430f9a361
JWHAPO <kim2101714@gmail.com>
2021-05-17 11:48:33 +0900

    add test controller

--- src/main/kotlin/com/yesfranchise/excel/ExcelApplication.kt
+++ src/main/kotlin/com/yesfranchise/excel/ExcelApplication.kt
@@ -1,9 +1,11 @@
 package com.yesfranchise.excel
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude= [DataSourceAutoConfiguration::class])
 class ExcelApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/yesfranchise/excel/controller/Hello.kt
@@ -0,0 +1,15 @@
+package com.yesfranchise.excel.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Hello {
+
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String {
+        return String.format("Hello %s!", name)
+    }
+
+}
